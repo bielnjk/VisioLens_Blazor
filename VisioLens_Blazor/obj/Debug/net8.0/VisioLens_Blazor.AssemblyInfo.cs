f3cc99bfde272747b855504163de552a20971c45
JeovanaKno <knoblauchjeovana6@gmail.com>
2025-09-30 16:37:51 -0400

    Atvidades 30/09 Jeovana

--- VisioLens_Blazor/obj/Debug/net8.0/VisioLens_Blazor.AssemblyInfo.cs
+++ VisioLens_Blazor/obj/Debug/net8.0/VisioLens_Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisioLens_Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a21c68d87e031a1fa8082251bed339c130d4644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923c9a98c6bebd4e9c206908620edc5faeb5bbca")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisioLens_Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisioLens_Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
